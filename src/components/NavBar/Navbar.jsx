3a0260388c2547fc1f0ae628bd974ab476371350
viniciusucanutx <viniciuscanutmz@gmail.com>
2024-09-25 15:32:49 -0300

    :sparkles: feat: Navbar do projeto

---
+++ src/components/NavBar/Navbar.jsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import React from 'react';
+import { Link, useNavigate } from "react-router-dom";
+import { BiCameraMovie, BiSearchAlt2 } from "react-icons/bi";
+import './Navbar.css';
+
+const Navbar = () => {
+    const [navActive, setNavActive] = useState(false);
+    const [search, setSearch] = useState("");
+    const navigate = useNavigate();
+
+    const toggleNav = () => {
+        setNavActive(!navActive);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        if (!search) return
+        navigate(`/search?q=${search}`, { replace: true });
+        setSearch("");
+    };
+
+    return (
+        <nav className="navbar">
+            <h2>
+                <img src="https://pt.boxcritters.wiki/images/3/38/Chap%C3%A9u_Pirata_%C3%ADcone.png" alt="" />
+            </h2>
+            <ul className={`nav-links ${navActive ? 'nav-active' : ''}`}>
+                    <a href="/" className="dropbtn">Home</a>
+                    <a href="/allmovies" className="dropbtn">Movies</a>
+                    <a href="/allseries" className="dropbtn">Series</a>
+                    </ul>
+            <form id='form01' onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    placeholder=""
+                    onChange={(e) => setSearch(e.target.value)}
+                    value={search}
+                />
+                <button type="submit">
+                    <BiSearchAlt2 />
+                </button>
+            </form>
+        </nav>
+    );
+};
+
+export default Navbar;
