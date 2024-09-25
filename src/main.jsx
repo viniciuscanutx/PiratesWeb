797ba29f680533f15da2d3376c3a80e0d2f0ab7a
viniciusucanutx <viniciuscanutmz@gmail.com>
2024-09-25 15:55:05 -0300

    :bricks: ci: Modificação nas rotas do projeto

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,30 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import App from './App';
+import Home from './pages/Home/Home';
+import Movie from './pages/MovieInfo/MovieInfo';
+import Player from './pages/Player/Player';
+import Search from './pages/Search/Search';
+import AllMovies from './pages/Allmovies/Allmovies';
+import Allseries from './pages/Allseries/Allseries';
+import Categories from './pages/Categories/Categories';
+import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route element={<App />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/:id" element={<Movie />} />
+          <Route path="/player/:id" element={<Player />} />
+          <Route path="search" element={<Search />} />
+          <Route path="allmovies" element={<AllMovies />} />
+          <Route path="allseries" element={<Allseries />} />
+          <Route path="/genero/:genero" element={<Categories />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
-)
+);
